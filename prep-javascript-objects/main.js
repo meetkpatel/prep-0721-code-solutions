f8d036da528ae269b94e7c3851e9597474a4a294
Meet Patel <mr.meetkpatel@gmail.com>
2021-08-11 17:41:14 -0700

    Changes done for prep-javascript-objects.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -5,13 +5,13 @@ var person = {
 };
 
 console.log(person);
-console.log(`The Person's name is: ${person.firstName}`);
+console.log("The Person's name is: " + person.firstName + ' ' + person.lastName);
 
 person.job = 'Learning';
-console.log(`The Person's current job is: ${person.job}`);
+console.log("The Person's current job is: " + person.job);
 
 person.previousJob = 'Cable Service Provider';
-console.log(`The Person's Previous job is: ${person.previousJob}`);
+console.log("The Person's Previous job is: " + person.previousJob);
 
 console.log('The Complete Person object:');
 console.table(person);
